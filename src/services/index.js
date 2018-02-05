24f44ce2a53b6fcbeccf6d2c5848c4556d4d5785
Simon Renoult <simon.renoult@gmail.com>
2018-02-05 09:24:30 +0100

    refactor: remove unused utils

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,5 @@
 module.exports = (logger, schemas, models) => {
   return {
-    // utils: require("./utils")(logger),
     bills: require("./bills")(schemas, models),
     products: require("./products")(schemas, models),
     orders: require("./orders")(schemas, models)
